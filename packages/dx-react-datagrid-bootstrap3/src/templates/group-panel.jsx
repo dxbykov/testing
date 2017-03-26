f0f12c7c18b1cca53a539e6df23fe54c18dd47d3
bykov <dxbykov@users.noreply.github.com>
2017-03-26 21:27:43 +0300

    Use bootstrap panel & buttons for group panel

--- packages/dx-react-datagrid-bootstrap3/src/templates/group-panel.jsx
+++ packages/dx-react-datagrid-bootstrap3/src/templates/group-panel.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
-export const GroupPanel = ({ grouping, groupByColumn }) => {
+export const GroupPanel = ({ grouping, groupByColumn, groupByColumnText }) => {
+    const text = groupByColumnText || 'Click G in the header to group by column';
     return (
-        <div
-            style={{ border: '1px solid black', }}>
-            {grouping.map(({ column: columnName }) =>
-                <div
-                    key={columnName}
-                    onClick={() => groupByColumn({ columnName, groupIndex: -1 })}
-                    style={{ 
-                        display: 'inline-block',
-                        border: '1px solid black',
-                        padding: '3px'
-                    }}>
-                    {columnName}
-                </div>
-            )}
+        <div className="panel panel-default" style={{ borderWidth: '1px 0', borderRadius: 0 }}>
+            <div className="panel-body">
+                {grouping.length
+                    ? grouping.map(({ column: columnName }) => 
+                        <button
+                            type="button"
+                            className="btn btn-default"
+                            key={columnName}
+                            onClick={() => groupByColumn({ columnName, groupIndex: -1 })} 
+                            style={{ 
+                                marginRight: '5px'
+                            }} >
+                            {columnName}
+                        </button>)
+                    : <span>{text}</span>
+                }
+            </div>
         </div>
     );
 }
