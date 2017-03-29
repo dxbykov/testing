a5bdfb1a3df76316ec1d38fb8f41fdb79217e936
bykov <dxbykov@users.noreply.github.com>
2017-03-29 15:37:30 +0300

    Setup linter on dx-core and fix issues

--- packages/dx-core/src/plugin-host.js
+++ packages/dx-core/src/plugin-host.js
@@ -1,52 +1,50 @@
 export class PluginHost {
-    constructor() {
-        this._plugins = [];
-        this._subscriptions = [];
-        this._gettersCache = {};
+  constructor() {
+    this.plugins = [];
+    this.subscriptions = [];
+    this.gettersCache = {};
+  }
+  registerPlugin(plugin) {
+    this.plugins.push(plugin);
+    this.cleanPluginsCache();
+  }
+  unregisterPlugin(plugin) {
+    this.plugins.splice(this.plugins.indexOf(plugin), 1);
+    this.cleanPluginsCache();
+  }
+  cleanPluginsCache() {
+    this.gettersCache = {};
+  }
+  collect(key, upTo) {
+    if (!this.gettersCache[key]) {
+      this.gettersCache[key] = this.plugins.map(plugin => plugin[key]).filter(plugin => !!plugin);
     }
+    if (!upTo) return this.gettersCache[key];
 
-    registerPlugin(plugin) {
-        this._plugins.push(plugin);
-        this.cleanPluginsCache();
-    }
-    unregisterPlugin(plugin) {
-        this._plugins.splice(this._plugins.indexOf(plugin), 1);
-        this.cleanPluginsCache();
-    }
-    cleanPluginsCache() {
-        this._gettersCache = {};
-    }
-    collect(key, upTo) {
-        if(!this._gettersCache[key]) {
-            this._gettersCache[key] = this._plugins.map(plugin => plugin[key]).filter(plugin => !!plugin);
-        }
-        if(!upTo) return this._gettersCache[key];
+    const upToIndex = this.plugins.indexOf(upTo);
+    return this.gettersCache[key].filter((getter) => {
+      const pluginIndex = this.plugins.findIndex(plugin => plugin[key] === getter);
+      return pluginIndex < upToIndex;
+    });
+  }
+  get(key, upTo) {
+    const plugins = this.collect(key, upTo);
 
-        const upToIndex = this._plugins.indexOf(upTo);
-        return this._gettersCache[key].filter(getter => {
-            const pluginIndex = this._plugins.findIndex(plugin => plugin[key] === getter);
-            return pluginIndex < upToIndex;
-        });
-    }
-    get(key, upTo) {
-        let plugins = this.collect(key, upTo);
-        
-        let result = plugins[0]();
-        plugins.slice(1).forEach(plugin => result = plugin(result));
-        return result;
-    }
-
-    registerSubscription(subscription) {
-        let index = this._subscriptions.indexOf(subscription);
-        if(index === -1)
-            this._subscriptions.push(subscription);
-    }
-    unregisterSubscription(subscription) {
-        let index = this._subscriptions.indexOf(subscription);
-        if(index !== -1)
-            this._subscriptions.splice(this._subscriptions.indexOf(subscription), 1);
-    }
-    broadcast(event, message) {
-        this._subscriptions.forEach(subscription => subscription[event] && subscription[event](message));
-    }
+    let result = plugins[0]();
+    plugins.slice(1).forEach((plugin) => {
+      result = plugin(result);
+    });
+    return result;
+  }
+  registerSubscription(subscription) {
+    const index = this.subscriptions.indexOf(subscription);
+    if (index === -1) { this.subscriptions.push(subscription); }
+  }
+  unregisterSubscription(subscription) {
+    const index = this.subscriptions.indexOf(subscription);
+    if (index !== -1) { this.subscriptions.splice(this.subscriptions.indexOf(subscription), 1); }
+  }
+  broadcast(event, message) {
+    this.subscriptions.forEach(subscription => subscription[event] && subscription[event](message));
+  }
 }
--- packages/dx-core/src/plugin-host.test.js
+++ packages/dx-core/src/plugin-host.test.js
@@ -1,129 +1,128 @@
 import { PluginHost } from './plugin-host';
 
 describe('PluginHost', () => {
-    let host;
+  let host;
 
-    beforeEach(() => {
-        host = new PluginHost();
-    });
+  beforeEach(() => {
+    host = new PluginHost();
+  });
 
-    test('#get', () => {
-        let plugin = {
-            something: () => 123
-        };
-        
-        host.registerPlugin(plugin);
-        expect(host.get('something')).toBe(123);
-    });
+  test('#get', () => {
+    const plugin = {
+      something: () => 123,
+    };
 
-    test('#get with extender', () => {
-        let plugin1 = {
-            something: () => '1'
-        };
-        let plugin2 = {
-            something: original => original + '2'
-        };
-        let plugin3 = {
-            something: original => original + '3'
-        };
-        
-        host.registerPlugin(plugin1);
-        host.registerPlugin(plugin2);
-        host.registerPlugin(plugin3);
-        expect(host.get('something')).toBe('123');
-    });
+    host.registerPlugin(plugin);
+    expect(host.get('something')).toBe(123);
+  });
 
-    test('#get clean cache', () => {
-        let plugin1 = {
-            something: () => '1'
-        };
-        let plugin2 = {
-            something: original => original + '2'
-        };
-        
-        host.registerPlugin(plugin1);
-        host.registerPlugin(plugin2);
-        host.get('something');
+  test('#get with extender', () => {
+    const plugin1 = {
+      something: () => '1',
+    };
+    const plugin2 = {
+      something: original => `${original}2`,
+    };
+    const plugin3 = {
+      something: original => `${original}3`,
+    };
 
-        host.unregisterPlugin(plugin2);
-        expect(host.get('something')).toBe('1');
+    host.registerPlugin(plugin1);
+    host.registerPlugin(plugin2);
+    host.registerPlugin(plugin3);
+    expect(host.get('something')).toBe('123');
+  });
 
-        host.registerPlugin(plugin2);
-        expect(host.get('something')).toBe('12');
-    });
+  test('#get clean cache', () => {
+    const plugin1 = {
+      something: () => '1',
+    };
+    const plugin2 = {
+      something: original => `${original}2`,
+    };
 
-    test('#collect', () => {
-        let plugin1 = {
-            something: 1
-        };
-        let plugin2 = {
-            something: 2
-        };
-        let plugin3 = {
-            something: 3
-        };
-        
-        host.registerPlugin(plugin1);
-        host.registerPlugin(plugin2);
-        host.registerPlugin(plugin3);
-        expect(host.collect('something')).toEqual([1, 2, 3]);
-    });
+    host.registerPlugin(plugin1);
+    host.registerPlugin(plugin2);
+    host.get('something');
 
-    test('#registerSubscription', () => {
-        let subscription = {
-            onMessage: jest.fn()
-        };
-        
-        host.registerSubscription(subscription);
-        host.broadcast('onMessage', 'update');
-        expect(subscription.onMessage.mock.calls.length).toBe(1);
-    });
+    host.unregisterPlugin(plugin2);
+    expect(host.get('something')).toBe('1');
 
-    test('#registerSubscription several times', () => {
-        let subscription = {
-            onMessage: jest.fn()
-        };
-        
-        host.registerSubscription(subscription);
-        host.registerSubscription(subscription);
-        host.broadcast('onMessage', 'update');
-        expect(subscription.onMessage.mock.calls.length).toBe(1);
-    });
+    host.registerPlugin(plugin2);
+    expect(host.get('something')).toBe('12');
+  });
 
-    test('#unregisterSubscription', () => {
-        let subscription = {
-            onMessage: jest.fn()
-        };
-        
-        host.registerSubscription(subscription);
-        host.unregisterSubscription(subscription);
-        host.broadcast('onMessage', 'update');
-        expect(subscription.onMessage.mock.calls.length).toBe(0);
-    });
+  test('#collect', () => {
+    const plugin1 = {
+      something: 1,
+    };
+    const plugin2 = {
+      something: 2,
+    };
+    const plugin3 = {
+      something: 3,
+    };
 
-    test('#unregisterSubscription without registration', () => {
-        let subscription1 = {
-            onMessage: jest.fn()
-        };
-        let subscription2 = {
-            onMessage: jest.fn()
-        };
-        
-        host.registerSubscription(subscription1);
-        host.unregisterSubscription(subscription2);
-        host.broadcast('onMessage', 'update');
-        expect(subscription1.onMessage.mock.calls.length).toBe(1);
-    });
+    host.registerPlugin(plugin1);
+    host.registerPlugin(plugin2);
+    host.registerPlugin(plugin3);
+    expect(host.collect('something')).toEqual([1, 2, 3]);
+  });
 
-    test('#registerSubscription', () => {
-        let subscription = {
-            onMessage: jest.fn()
-        };
-        
-        host.registerSubscription(subscription);
-        host.broadcast('onMessage', 'update');
-        expect(subscription.onMessage.mock.calls[0].length).toBe(1);
-        expect(subscription.onMessage.mock.calls[0][0]).toBe('update');
-    });
+  test('#registerSubscription', () => {
+    const subscription = {
+      onMessage: jest.fn(),
+    };
 
+    host.registerSubscription(subscription);
+    host.broadcast('onMessage', 'update');
+    expect(subscription.onMessage.mock.calls.length).toBe(1);
+  });
+
+  test('#registerSubscription several times', () => {
+    const subscription = {
+      onMessage: jest.fn(),
+    };
+
+    host.registerSubscription(subscription);
+    host.registerSubscription(subscription);
+    host.broadcast('onMessage', 'update');
+    expect(subscription.onMessage.mock.calls.length).toBe(1);
+  });
+
+  test('#unregisterSubscription', () => {
+    const subscription = {
+      onMessage: jest.fn(),
+    };
+
+    host.registerSubscription(subscription);
+    host.unregisterSubscription(subscription);
+    host.broadcast('onMessage', 'update');
+    expect(subscription.onMessage.mock.calls.length).toBe(0);
+  });
+
+  test('#unregisterSubscription without registration', () => {
+    const subscription1 = {
+      onMessage: jest.fn(),
+    };
+    const subscription2 = {
+      onMessage: jest.fn(),
+    };
+
+    host.registerSubscription(subscription1);
+    host.unregisterSubscription(subscription2);
+    host.broadcast('onMessage', 'update');
+    expect(subscription1.onMessage.mock.calls.length).toBe(1);
+  });
+
+  test('#registerSubscription', () => {
+    const subscription = {
+      onMessage: jest.fn(),
+    };
+
+    host.registerSubscription(subscription);
+    host.broadcast('onMessage', 'update');
+    expect(subscription.onMessage.mock.calls[0].length).toBe(1);
+    expect(subscription.onMessage.mock.calls[0][0]).toBe('update');
+  });
 });
