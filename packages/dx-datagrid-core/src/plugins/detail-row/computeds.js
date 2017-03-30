3b8697f9855d9428b3e7b57586ecad8c6bb72234
bykov <dxbykov@users.noreply.github.com>
2017-03-30 12:08:37 +0300

    Fix detail row expanding issue

--- packages/dx-datagrid-core/src/plugins/detail-row/computeds.js
+++ packages/dx-datagrid-core/src/plugins/detail-row/computeds.js
@@ -1,17 +1,17 @@
-export const expandedDetailRows = (rows, expandedRows) => {
-  let result = rows;
+export const expandedDetailRows = (sourceRows, expandedRows) => {
+  let rows = sourceRows;
   expandedRows.filter((value, index, self) => self.indexOf(value) === index).forEach((rowId) => {
     const index = rows.findIndex(row => row.id === rowId);
     if (index !== -1) {
       const rowIndex = rows.findIndex(row => row.id === rowId);
       const insertIndex = rowIndex + 1;
       const row = rows[rowIndex];
-      result = [
+      rows = [
         ...rows.slice(0, insertIndex),
-                { type: 'detailRow', id: `detailRow_${row.id}`, for: row, colspan: 0, height: 'auto' },
+        { type: 'detailRow', id: `detailRow_${row.id}`, for: row, colspan: 0, height: 'auto' },
         ...rows.slice(insertIndex),
       ];
     }
   });
-  return result;
+  return rows;
 };
